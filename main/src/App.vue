8100b4c27f91b60651cc261cc28cf7a985d25f45
The_Valkyrie <110331611+Kirieshek@users.noreply.github.com>
2024-01-20 13:45:06 +0300

    Add files via upload

---
+++ main/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input v-model="inputText" type="text" placeholder="Введите текст">
+    <button @click="addItem">Добавить</button>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputText: '',
+      items: []
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.inputText !== '') {
+
+        // this.items.push(this.inputText); -- в начало списка
+        // this.items.unshift(this.inputText) -- в конец списка
+
+        // Евгений Денисович, просто откомментируйте одну из строчек, чтобы увидеть результат
+
+        this.inputText = '';
+      }
+    } 
+  }
+};
+</script>
